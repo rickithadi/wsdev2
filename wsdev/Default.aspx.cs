f22b33c0385058b3bbc8f281f97e047a1866c87f
rickithadi <rickithadi@gmail.com>
2018-01-23 15:36:11 +0800

    initial

---
+++ wsdev/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using wsdev.financeWS;
+
+namespace wsdev
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        FinanceService Fservice = new FinanceService();
+        double LoanAmount, ExtraCost, InterestRate, Months,ResidualValue,MonthlyPayment;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double apr = Fservice.APR(LoanAmount, ExtraCost, InterestRate, Months);
+            double leasemp = Fservice.LeaseMonthlyPayment(LoanAmount, ResidualValue, InterestRate, Months);
+            double loanmp = Fservice.LoanMonthlyPayment(LoanAmount, InterestRate, Months);
+            double loanNum = Fservice.LoanNumberOfPayment(LoanAmount, InterestRate, MonthlyPayment);
+
+
+
+        }
+    }
+}
